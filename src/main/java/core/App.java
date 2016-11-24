89fbbe2e488dd0b1bd4c561e4a0fccbc4cb90735
YOUR NAME <YOUR EMAIL ADDRESS>
2016-11-23 19:38:04 -0800

    HM 39

---
+++ src/main/java/core/App.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+  
+        
+    	
+        public static String  s = "JUnit 4.12";
+        public static Integer i = 39;
+        public static Boolean t = true;
+        public static Boolean f = false;
+        
+    }
+
